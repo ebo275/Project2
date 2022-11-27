dd5ff92869802f57cd2ff3612386a65cc7d666ce
ebo275 <115588483+ebo275@users.noreply.github.com>
2022-11-27 12:59:36 -0800

    tried jquery didnt work

--- public/app.js
+++ public/app.js
@@ -1,3 +1,18 @@
 $(()=> {
         console.log('js connected properly')
+
+        const $addBtn = $('#add-btn')
+        const $addModal = $('#add-modal')
+
+        const modalSlide = () => {
+                $addModal.css('display', 'flex')
+        }
+
+        const modalHide = () => {
+                $addModal.css('display', 'none')
+        }
+
+        $addBtn.mouseover(modalSlide)
+        
     })
+    
